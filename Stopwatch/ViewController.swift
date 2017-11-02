4d91cadc6ba145343f8a5ab22f3d3dfdd61a9761
miwaterutada <teltoraman30@gmail.com>
2017-11-02 19:27:16 +0900

    かんりょうしました

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -3,35 +3,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    //カウントする初期数値を表す変数
     var countNum = 0
-    //カウントする初期数値
-    var timerRunning = false
-    //最初はカウントをしないようにする
-    var timer = Timer()
-    //
     
-    func update() {
+    //タイマーが動いているかどうかを保持する変数
+    var timerRunning = false
+    
+    //タイマーを管理する変数
+    var timer = Timer()
+    
+    @objc
+    func update(sender: Timer) {
+        //１ずつカウントしていく
         countNum += 1
+        //1づつカウントしていく数値をlabelに表示させる
         label.text = "\(countNum)"
     }
     
-    
     @IBOutlet weak var label: UILabel!
     
-   
+    
     @IBAction func start(_ sender: Any) {
+        //もしタイマーが動いてない時
         if !timerRunning {
-            
+            // １秒間隔で繰り返し、ViewController.update(sender:)を呼び出すタイマーを定義
+            timer = Timer.scheduledTimer(timeInterval: 1,
+                                         target: self,
+                                         selector: #selector(ViewController.update(sender:)),
+                                         userInfo: nil,
+                                         repeats: true)
+            timerRunning = true
         }
     }
     
     @IBAction func stop(_ sender: Any) {
-        
+        //もしタイマーが動いている時
+        if timerRunning {
+            //タイマーを無効にする
+            timer.invalidate()
+            //タイマーは止まってます
+            timerRunning = false
+        }
     }
-    
     @IBAction func reset(_ sender: Any) {
-        
+        //カウントする初期値を表示する
+        countNum = 0
+        //countNumをlabelの文字列へ代入する
+        label.text = "\(countNum)"
     }
     
     override func viewDidLoad() {
